3f59afaadfa2a6a61423f56cab93c6df220e5377
alirhaby <46032412+alirhaby@users.noreply.github.com>
2019-03-29 20:23:40 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js')
 const Client = new Discord.Client()
 
-Client.login('You'r Token').then(async () => {
+Client.login('NDU1Mjg4Mzk4MjQ5MzI4NjUw.D3qryw.hcv1H3wwx75-r5W80b4rafqK-Dc').then(async () => {
     console.log(`Ligado em ${Client.user.tag}`)
 })
 Client.on('message', async message => {
-    if (message.author.id === 'You'r ID') {
+    if (message.author.id === '455288398249328650') {
         let prefix = '!'
         const args = message.content.slice(prefix.length).trim().split(' ') 
         if (message.content.startsWith(`${prefix}avatar`)) {
